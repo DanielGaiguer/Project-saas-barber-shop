41f6d5a894642ff976ead3219569745dc6ef0b0d
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-09 15:35:50 -0300

    Configure shdcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./_components/ui/button"
+
 const Home = () => {
-  return <h1 className="mb-5 bg-red-400 text-red-500">Home Page</h1> 
+  return <Button></Button>
 }
 
 export default Home
