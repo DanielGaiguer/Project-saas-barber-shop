4c5122e875e4d5cb08555aad05538038e94d6e21
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-08 21:25:07 -0300

    create schema

---
+++ prisma/migrations/20260109001624_init_db/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Booking` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Booking" DROP CONSTRAINT "Booking_id_fkey";
+
+-- AlterTable
+ALTER TABLE "Booking" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
