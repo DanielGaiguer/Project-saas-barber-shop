bd7cc1ef0a66e601eb7699af597fc8d902631d48
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-12 14:13:29 -0300

    feat: add component footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,6 +96,15 @@ const Home = async () => {
           ))}
         </div>
       </div>
+      <footer>
+        <Card className="px-5 py-6">
+          <CardContent>
+            <p className="text-sm text-gray-400">
+              © 2025 Copyright <span className="font-bold">Daniel Gaiguer</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
