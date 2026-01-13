99da4831db92bf37aae352759d82872ff96f4abf
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-13 11:18:39 -0300

    fix: bug with prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,9 @@ import "dotenv/config"
 import { defineConfig } from "prisma/config"
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: { path: "prisma/migrations", seed: "tsx prisma/seed.ts" },
+  migrations: {
+    path: "prisma/migrations",
+    seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
+  },
   datasource: { url: process.env["DATABASE_URL"] },
 })
