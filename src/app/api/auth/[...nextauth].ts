e4e4c56b98f015ca9d5656721faa4a80da621a6d
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-13 11:43:27 -0300

    feat: add adapter

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,11 +1,18 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { db } from "../../_lib/prisma" // seu PrismaClient com adapter customizado
+import { Adapter } from "next-auth/adapters"
 
 export default NextAuth({
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  session: {
+    strategy: "database",
+  },
 })
