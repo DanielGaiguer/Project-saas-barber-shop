efdc9572d8f22039b4b0791a090b459b8024762d
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-14 09:17:39 -0300

    feat: ensure babreshops can be searched by title and service

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,17 +1,29 @@
-import { Card, CardContent } from "./ui/card"
+// import { Card, CardContent } from "./ui/card"
 
 const Footer = () => {
   return (
-    <footer>
-      <Card className="px-5 py-6">
-        <CardContent>
-          <p className="text-sm text-gray-400">
-            © 2025 Copyright <span className="font-bold">Daniel Gaiguer</span>
-          </p>
-        </CardContent>
-      </Card>
+    <footer className="shrink-0">
+      <div className="border-t px-5 py-6">
+        <p className="text-sm text-gray-400">
+          © 2025 Copyright <span className="font-bold">Daniel Gaiguer</span>
+        </p>
+      </div>
     </footer>
   )
 }
+
+// const Footer = () => {
+//   return (
+//     <footer>
+//       <Card className="px-5 py-6">
+//         <CardContent>
+//           <p className="text-sm text-gray-400">
+//             © 2025 Copyright <span className="font-bold">Daniel Gaiguer</span>
+//           </p>
+//         </CardContent>
+//       </Card>
+//     </footer>
+//   )
+// }
 
 export default Footer
