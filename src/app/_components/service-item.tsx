7e672f9cf68d659e1c3ffcc21fac89b9b4be8625
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-16 17:36:06 -0300

    feat: add see bookings button on booking sucess alert

--- src/app/_components/service-item.tsx
+++ src/app/_components/service-item.tsx
@@ -24,6 +24,7 @@ import { getBookings } from "../_actions/get-bookings"
 import { Dialog, DialogContent } from "./ui/dialog"
 import SignInDialog from "./sign-in-dialog"
 import BookingSummary from "./booking-summary"
+import { useRouter } from "next/navigation"
 
 interface ServiceItemProps {
   service: BarbershopService
@@ -84,6 +85,8 @@ const getTimeList = ({ bookings, selectedDay }: GetTimeListProps) => {
 
 const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
   const { data } = useSession()
+
+  const router = useRouter()
 
   const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined)
 
@@ -148,7 +151,12 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
         date: selectedDate,
       })
       handleBookingSheetsOpenChange()
-      toast.success("Reserva criada com sucesso!")
+      toast.success("Reserva criada com sucesso!", {
+        action: {
+          label: "Ver agendamentos",
+          onClick: () => router.push("/bookings"),
+        },
+      })
     } catch (e) {
       console.log(e)
       toast.error("Erro ao criar reserva.")
